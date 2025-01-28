607fa05b53933387b7ff30c0296255dbc0738cc6
xhcx-ale <alexzpta8@gmail.com>
2025-01-27 19:33:38 -0600

    crons fxd

--- src/helpers/cron.js
+++ src/helpers/cron.js
@@ -1,7 +1,7 @@
 import cron from 'cron'
 import https from 'https'
 
-const url = 'http:localhost:4000'
+const url = 'https://instools.onrender.com'
 const job = new cron.CronJob('*/14 * * * *', function() {
   console.log('Reiniciando...')
   https
